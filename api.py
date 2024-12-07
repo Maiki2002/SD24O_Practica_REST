ef07ee1ea7c322ae1d40496c8abd77cc653b2717
Maiki2002 <maiki200202@gmail.com>
2024-12-07 15:04:47 -0600

    Se implementó las consultas select a las tablas usuario, fotos y calificaciones

--- api.py
+++ api.py
@@ -18,3 +18,18 @@ def hola_mundo():
     }
 
     return respuesta
+
+@app.get("/alumnos")
+def lista_alumnos(sesion:Session=Depends(generar_sesion)):
+    print("Consultando todos los alumnos")
+    return repo.devuelve_alumnos(sesion)
+
+@app.get("/fotos")
+def lista_fotos(sesion:Session=Depends(generar_sesion)):
+    print("Consultando todas las fotos")
+    return repo.devuelve_fotos(sesion)
+
+@app.get("/calificaciones")
+def lista_calificaciones(sesion:Session=Depends(generar_sesion)):
+    print("Consultando todas las calificaciones")
+    return repo.devuelve_calificaciones(sesion)
--- orm/repo.py
+++ orm/repo.py
@@ -2,5 +2,14 @@ import orm.modelos as modelos
 from sqlalchemy.orm import Session
 from sqlalchemy import and_
 
+def devuelve_alumnos(sesion:Session):
+    print("select * from app.alumnos")
+    return sesion.query(modelos.Alumno).all()
 
+def devuelve_fotos(sesion:Session):
+    print("select * from app.fotos")
+    return sesion.query(modelos.Foto).all()
 
+def devuelve_calificaciones(sesion:Session):
+    print("select * from app.calificaciones")
+    return sesion.query(modelos.Calificacion).all()
